6a0350d5f29112d3d36bf1197b84734992210f0d
Jordan Riley <30568107+jordanbr20@users.noreply.github.com>
2024-05-20 10:02:53 -0400

    Create COLORS BY BACKGROUND.bat

---
+++ COLORS BY BACKGROUND.bat
@@ -0,0 +1,422 @@
+ECHO OFF 
+CLS
+
+COLOR 0A
+ECHO COLOR 0A
+ECHO 0 - Black Background
+ECHO A - Light Green Text
+PAUSE 
+CLS
+
+COLOR 0B
+ECHO COLOR 0B
+ECHO 0 - Black Background 
+ECHO B - Light Aqua Text
+PAUSE 
+CLS 
+
+COLOR 0C
+ECHO COLOR 0C
+ECHO 0 - Black Background 
+ECHO C - Light Red
+PAUSE 
+CLS 
+
+COLOR 0D
+ECHO COLOR 0D
+ECHO 0 - Black Background 
+ECHO D - Light Purple Text
+PAUSE 
+CLS 
+
+COLOR 0E
+ECHO COLOR 0E
+ECHO 0 - Black Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 0F
+ECHO COLOR 0F
+ECHO 0 - Black Background 
+ECHO F - Bright White Text
+PAUSE 
+CLS 
+
+COLOR 1A
+ECHO COLOR 1A
+ECHO 1 - Blue Background
+ECHO A - Light Green Text
+PAUSE 
+CLS
+
+COLOR 1B
+ECHO COLOR 1B
+ECHO 1 - Blue Background 
+ECHO B - Light Aqua Text
+PAUSE 
+CLS 
+
+COLOR 1C
+ECHO COLOR 1C
+ECHO 1 - Blue Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 1D
+ECHO COLOR 1D
+ECHO 1 - Blue Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 1E
+ECHO COLOR 1E
+ECHO 1 - Blue Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 1F
+ECHO COLOR 1F
+ECHO 1 - Blue Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 2A
+ECHO COLOR 2A
+ECHO 2 - Green Background
+ECHO A - Light Green Text
+PAUSE 
+CLS 
+
+COLOR 2B
+ECHO COLOR 2B
+ECHO 2 - Green Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 2C
+ECHO COLOR 2C
+ECHO 2 - Green Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 2D
+ECHO COLOR 2D
+ECHO 2 - Green Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 2E
+ECHO COLOR 2E
+ECHO 2 - Green Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 2F
+ECHO COLOR 2F
+ECHO 2 - Green Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 3A
+ECHO COLOR 3A
+ECHO 3 - Aqua Background
+ECHO A - Light Green Text
+PAUSE 
+CLS 
+
+COLOR 3B
+ECHO COLOR 3B
+ECHO 3 - Aqua Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 3C
+ECHO COLOR 3C
+ECHO 3 - Aqua Background
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 3D
+ECHO COLOR 3D
+ECHO 3 - Aqua Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 3E
+ECHO COLOR 3E
+ECHO 3 - Aqua Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 3F
+ECHO COLOR 3F
+ECHO 3 - Aqua Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 4A
+ECHO COLOR 4A
+ECHO 4 - Red Background
+ECHO A - Light Green Text
+PAUSE 
+CLS 
+
+COLOR 4B
+ECHO COLOR 4B
+ECHO 4 - Red Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 4C
+ECHO COLOR 4C
+ECHO 4 - Red Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 4D
+ECHO COLOR 4D
+ECHO 4 - Red Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 4E
+ECHO COLOR 4E
+ECHO 4 - Red Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 4F
+ECHO COLOR 4F
+ECHO 4 - Red Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 5A
+ECHO COLOR 5A
+ECHO 5 - Purple Background
+ECHO A - Light Green Text 
+PAUSE 
+CLS 
+
+COLOR 5B
+ECHO COLOR 5B
+ECHO 5 - Purple Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 5C
+ECHO COLOR 5C
+ECHO 5 - Purple Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 5D
+ECHO COLOR 5D
+ECHO 5 - Purple Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 5E
+ECHO COLOR 5E
+ECHO 5 - Purple Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 5F
+ECHO COLOR 5F
+ECHO 5 - Purple Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 6A
+ECHO COLOR 6A
+ECHO 6 - Yellow Background
+ECHO A - Light Green Text 
+PAUSE 
+CLS 
+
+COLOR 6B
+ECHO COLOR 6B
+ECHO 6 - Yellow Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 6C
+ECHO COLOR 6C
+ECHO 6 - Yellow Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 6D
+ECHO COLOR 6D
+ECHO 6 - Yellow Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 6E
+ECHO COLOR 6E
+ECHO 6 - Yellow Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 6F
+ECHO COLOR 6F
+ECHO 6 - Yellow Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 7A
+ECHO COLOR 7A
+ECHO 7 - White Background
+ECHO A - Light Green Text 
+PAUSE 
+CLS 
+
+COLOR 7B
+ECHO COLOR 7B
+ECHO 7 - White Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 7C
+ECHO COLOR 7C
+ECHO 7 - White Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 7D
+ECHO COLOR 7D
+ECHO 7 - White Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 7E
+ECHO COLOR 7E
+ECHO 7 - White Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 7F
+ECHO COLOR 7F
+ECHO 7 - White Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 8A
+ECHO COLOR 8A
+ECHO 8 - Gray Background
+ECHO A - Light Green Text 
+PAUSE 
+CLS 
+
+COLOR 8B
+ECHO COLOR 8B
+ECHO 8 - Gray Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 8C
+ECHO COLOR 8C
+ECHO 8 - Gray Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 8D
+ECHO COLOR 8D
+ECHO 8 - Gray Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 8E
+ECHO COLOR 8E
+ECHO 8 - Gray Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 8F
+ECHO COLOR 8F
+ECHO 8 - Gray Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
+
+COLOR 9A
+ECHO COLOR 9A
+ECHO 9 - Light Blue Background
+ECHO A - Light Green Text 
+PAUSE 
+CLS 
+
+COLOR 9B
+ECHO COLOR 9B
+ECHO 9 - Light Blue Background 
+ECHO B - Light Aqua Text 
+PAUSE 
+CLS 
+
+COLOR 9C
+ECHO COLOR 9C
+ECHO 9 - Light Blue Background 
+ECHO C - Light Red Text
+PAUSE 
+CLS 
+
+COLOR 9D
+ECHO COLOR 9D
+ECHO 9 - Light Blue Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 9E
+ECHO COLOR 9E
+ECHO 9 - Light Blue Background 
+ECHO D - Light Purple Text 
+PAUSE 
+CLS 
+
+COLOR 9F
+ECHO COLOR 9F
+ECHO 9 - Light Blue Background 
+ECHO F - Bright White Text 
+PAUSE 
+CLS 
